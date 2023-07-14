945268177a5c3e12d30e0726696410d49aa2c3d7
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2023-07-14 10:44:57 -0600

    some tweaks

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+app.app_context().push()
 
 
 ##############################################################################
